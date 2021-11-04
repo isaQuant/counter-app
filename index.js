ebf7ca95a70bda0faf7625b9a935da0fbfd33083
isaQuant <isabella.quantschnig@gmx.net>
2021-11-04 16:26:42 +0100

    created functions for incrementing and decrementing the counter and created function for saving the current count

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-document.getElementById("count-el").innerText = 5
+//document.getElementById("count-el").innerText = 5
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0;
 
-// min 7:41
-//
+
+function increment() {
+    count++;
+    countEl.textContent = count;
+}
+
+function decrement() {
+    if(count > 0) {
+        count--;
+    }
+    countEl.innerText = count;
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+}
+
